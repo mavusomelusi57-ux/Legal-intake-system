0cd53ac5c336bc85b0ad28828c05786580696212
mavusomelusi57-ux <mavusomelusi57@gmail.com>
2026-07-15 15:41:23 +0200

    fixing server line 85 syntax

--- app.py
+++ app.py
@@ -83,4 +83,3 @@ def google_verify():
 if __name__ == '__main__':
     initialize_db()
     app.run(host='0.0.0.0' if os.environ.get('PORT') else '127.0.0.1', port=int(os.environ.get('PORT', 5000)), debug=True)
-
