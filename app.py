3d0c0a10f73c2f7f19c3143a1e405ff6b8d57d6c
mavusomelusi57-ux <mavusomelusi57@gmail.com>
2026-07-15 13:54:42 +0200

    added data viewer route

--- app.py
+++ app.py
@@ -28,6 +28,26 @@ def submit():
         
     return f"Thank you, {name}. Your consultation request for {service} has been received. E.M. Mavuso Attorneys will contact you shortly."
 
+@app.route('/view-consultations', methods=['GET'])
+def view_consultations():
+    data = []
+    if os.path.exists(DB_FILE):
+        with open(DB_FILE, mode='r', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            headers = next(reader, None)  # Skip the header row
+            for row in reader:
+                if row:  # Make sure the row isn't empty
+                    data.append(row)
+    
+    # Generate a quick, clean HTML table to look at the data
+    html_output = "<h2>Submitted Client Consultations</h2><table border='1' cellpadding='10'>"
+    html_output += "<tr><th>Name</th><th>Phone</th><th>Service</th><th>Message</th></tr>"
+    for row in data:
+        html_output += f"<tr><td>{row[0]}</td><td>{row[1]}</td><td>{row[2]}</td><td>{row[3]}</td></tr>"
+    html_output += "</table><br><a href='/'>Back to Home</a>"
+    
+    return html_output
+
 if __name__ == '__main__':
     initialize_db()
     app.run(host='0.0.0.0' if os.environ.get('PORT') else '127.0.0.1', port=int(os.environ.get('PORT', 5000)), debug=True)
